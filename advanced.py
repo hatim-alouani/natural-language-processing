801eb8e1557e680d6872aed100562d5c4ce96761
Hatim Alouani <haalouan@e2r7p6.1337.ma>
2025-06-01 21:12:13 +0100

    nlp

--- advanced.py
+++ advanced.py
@@ -1,37 +1,42 @@
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from gensim.models import Word2Vec, KeyedVectors, FastText
-import spacyPreProcessing
-from spacyPreProcessing import cleaning
+from gensim.models import Word2Vec, FastText
+import spacy
 
-def bow():
-    cv = CountVectorizer()
-    return cv
+english = spacy.load("en_core_web_sm")
 
-def TF_IDF():
-    tv = TfidfVectorizer()
-    return tv
-    
-def Word2Vec(sentences):
-    cbow =  Word2Vec(sentences=sentences)
-    return cbow
+def cleaning(text):
+    doc = english(text)
+    tokens = [token.lemma_.lower() for token in doc if token.is_alpha and not token.is_stop]
+    return ' '.join(tokens)
+
+def bowModel():
+    return CountVectorizer()
+
+def TF_IDF_Model():
+    return TfidfVectorizer()
+
+def Word2VecModel(sentences):
+    return Word2Vec(sentences=sentences, vector_size=100, window=5, min_count=1, workers=4)
 
 def FastTextModel(sentences):
-    ft = FastText(sentences=sentences, vector_size=200, window=3, min_count=1, workers=4)
-    return ft
+    return FastText(sentences=sentences, vector_size=100, window=3, min_count=1, workers=4)
 
-text = english('NLP is a fascinating technique! I started learning it 2 days ago.')
+text = 'NLP is a fascinating technique! I started learning it 2 days ago.'
 text = cleaning(text)
 
-cv = bow()
-tv = TF_IDF()
-cbow = Word2Vec(tokens)
-tf = FastText(tokens)
+tokens = [text.split()]
 
-x = cv.fit_transform(tokens)
-print(x.toarray())
-x = tv.fit_transform(tokens)
-print(x.toarray())
-x = cbow.predict(tokens)
-print(x.toarray())
-# x = 
-# print(x.toarray())
+cv = bowModel()
+tv = TF_IDF_Model()
+cbow = Word2VecModel(tokens)
+ft = FastTextModel(tokens)
+
+x = cv.fit_transform([text])
+print("BoW:\n", x.toarray())
+x = tv.fit_transform([text])
+print("TF-IDF:\n", x)
+text = text.split()
+x = cbow.predict_output_word(text, topn=5)
+print("Word2vec:\n", x)
+x = ft.predict_output_word(text, topn=5)
+print("Word2vec:\n", x)
--- spacyPreProcessing.py
+++ spacyPreProcessing.py
@@ -6,7 +6,7 @@ def cleaning(text):
     text = english(text)
     tokens = [token.lemma_.lower() for token in text if token.is_alpha and not token.is_stop]
     text = ' '.join(tokens)
-    return english(text)
+    return text
 
 text = 'NLP is a fascinating technique! I started learning it 2 days ago.'
 text = cleaning(text)
